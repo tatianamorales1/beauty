da9cd68685e516aa4e611568adf70cf7749f8fdd
gwc <gwc@gwcs-MacBook-Pro.local>
2017-08-22 16:20:51 -0400

    Add all files

---
+++ Beu+y/platforms/browser/www/css/userinput.js
@@ -0,0 +1,158 @@
+function save() {
+  var ginput = document.getElementById("genders");
+  localStorage.setItem("genders", ginput.value);
+  var getgenders = localStorage.getItem("genders");
+  console.log(getgenders);
+
+  if (document.getElementById("femaleOutfit1").checked == true){
+    document.getElementById("femaleOutfit1").value;
+    var fo1input = document.getElementById("femaleOutfit1");
+    localStorage.setItem("femaleOutfit1", fo1input.value);
+    var getfemaleOutfit1 = localStorage.getItem("femaleOutfit1");
+    console.log(getfemaleOutfit1);
+  }
+  else if (document.getElementById("femaleOutfit2").checked == true){
+    document.getElementById("femaleOutfit2").value;
+    var fo2input = document.getElementById("femaleOutfit2");
+    localStorage.setItem("femaleOutfit2", fo2input.value);
+    var getfemaleOutfit2 = localStorage.getItem("femaleOutfit2");
+    console.log(getfemaleOutfit2);
+  }
+  else if (document.getElementById("femaleOutfit3").checked == true){
+    document.getElementById("femaleOutfit3").value;
+    var fo3input = document.getElementById("femaleOutfit3");
+    localStorage.setItem("femaleOutfit3", fo3input.value);
+    var getfemaleOutfit3 = localStorage.getItem("femaleOutfit3");
+    console.log(getfemaleOutfit3);
+  }
+  else if (document.getElementById("femaleOutfit4").checked == true){
+    document.getElementById("femaleOutfit4").value;
+    var fo4input = document.getElementById("femaleOutfit4");
+    localStorage.setItem("femaleOutfit4", fo4input.value);
+    var getfemaleOutfit4 = localStorage.getItem("femaleOutfit4");
+    console.log(getfemaleOutfit4);
+  }
+  else if (document.getElementById("femaleOutfit5").checked == true){
+    document.getElementById("femaleOutfit5").value;
+    var fo5input = document.getElementById("femaleOutfit5");
+    localStorage.setItem("femaleOutfit5", fo5input.value);
+    var getfemaleOutfit5 = localStorage.getItem("femaleOutfit5");
+    console.log(getfemaleOutfit5);
+  }
+  else if (document.getElementById("femaleOutfit6").checked == true){
+    document.getElementById("femaleOutfit6").value;
+    var fo6input = document.getElementById("femaleOutfit6");
+    localStorage.setItem("femaleOutfit6", fo6input.value);
+    var getfemaleOutfit6 = localStorage.getItem("femaleOutfit6");
+    console.log(getfemaleOutfit6);
+  }
+
+
+
+
+
+  if (document.getElementById("maleOutfit1").checked == true){
+    document.getElementById("maleOutfit1").value;
+    var mo1input = document.getElementById("maleOutfit1");
+    localStorage.setItem("maleOutfit1", mo1input.value);
+    var getmaleOutfit1 = localStorage.getItem("maleOutfit1");
+    console.log(getmaleOutfit1);
+  }
+  else if (document.getElementById("maleOutfit2").checked == true){
+    document.getElementById("maleOutfit2").value;
+    var mo2input = document.getElementById("maleOutfit2");
+    localStorage.setItem("maleOutfit2", mo2input.value);
+    var getmaleOutfit2 = localStorage.getItem("maleOutfit2");
+    console.log(getmaleOutfit2);
+  }
+  else if (document.getElementById("maleOutfit3").checked == true){
+    document.getElementById("maleOutfit3").value;
+    var mo3input = document.getElementById("maleOutfit3");
+    localStorage.setItem("maleOutfit3", mo3input.value);
+    var getmaleOutfit3 = localStorage.getItem("maleOutfit3");
+    console.log(getmaleOutfit3);
+  }
+  else if (document.getElementById("maleOutfit4").checked == true){
+    document.getElementById("maleOutfit4").value;
+    var mo4input = document.getElementById("maleOutfit4");
+    localStorage.setItem("maleOutfit4", mo4input.value);
+    var getmaleOutfit4 = localStorage.getItem("maleOutfit4");
+    console.log(getmaleOutfit4);
+  }
+  else if (document.getElementById("maleOutfit5").checked == true){
+    document.getElementById("maleOutfit5").value;
+    var mo5input = document.getElementById("maleOutfit5");
+    localStorage.setItem("maleOutfit5", mo5input.value);
+    var getmaleOutfit5 = localStorage.getItem("maleOutfit5");
+    console.log(getmaleOutfit5);
+  }
+  else if (document.getElementById("maleOutfit6").checked == true){
+    document.getElementById("maleOutfit6").value;
+    var mo6input = document.getElementById("maleOutfit6");
+    localStorage.setItem("maleOutfit6", mo6input.value);
+    var getmaleOutfit6 = localStorage.getItem("maleOutfit6");
+    console.log(getmaleOutfit6);
+  }
+  if (document.getElementById("a1").checked == true){
+    document.getElementById("a1").value;
+    var age1input = document.getElementById("a1");
+    localStorage.setItem("a1", age1input.value);
+    var getage1input = localStorage.getItem("a1");
+    console.log(getage1input);
+  }
+  else if (document.getElementById("a2").checked == true){
+    document.getElementById("a2").value;
+    var age2input = document.getElementById("a2");
+    localStorage.setItem("a2", age2input.value);
+    var getage2input = localStorage.getItem("a2");
+    console.log(getage2input);
+  }
+  else if (document.getElementById("a3").checked == true){
+    document.getElementById("a3").value;
+    var age3input = document.getElementById("a3");
+    localStorage.setItem("a3", age3input.value);
+    var getage3input = localStorage.getItem("a3");
+    console.log(getage3input);
+  }
+  else if (document.getElementById("a4").checked == true){
+    document.getElementById("a4").value;
+    var age4input = document.getElementById("a4");
+    localStorage.setItem("a4", age4input.value);
+    var getage4input = localStorage.getItem("a4");
+    console.log(getage4input);
+  }
+  else if (document.getElementById("a5").checked == true){
+    document.getElementById("a5").value;
+    var age5input = document.getElementById("a5");
+    localStorage.setItem("a5", age5input.value);
+    var getage5input = localStorage.getItem("a5");
+    console.log(getage5input);
+  }
+  else if (document.getElementById("a6").checked == true){
+    document.getElementById("a6").value;
+    var age6input = document.getElementById("a6");
+    localStorage.setItem("a6", age6input.value);
+    var getage6input = localStorage.getItem("a6");
+    console.log(getage6input);
+  }
+  else if (document.getElementById("a7").checked == true){
+    document.getElementById("a7").value;
+    var age7input = document.getElementById("a7");
+    localStorage.setItem("a7", age7input.value);
+    var getage7input = localStorage.getItem("a7");
+    console.log(getage7input);
+  }
+  else if (document.getElementById("a8").checked == true){
+    document.getElementById("a8").value;
+    var age8input = document.getElementById("a8");
+    localStorage.setItem("a8", age3input.value);
+    var getage8input = localStorage.getItem("a8");
+    console.log(getage8input);
+  }
+  else if (document.getElementById("a9").checked == true){
+    document.getElementById("a9").value;
+    var age9input = document.getElementById("a9");
+    localStorage.setItem("a9", age3input.value);
+    var getage9input = localStorage.getItem("a9");
+    console.log(getage9input);
+ }
---
+++ survey.js
@@ -0,0 +1,382 @@
+function save() {
+
+  var ginput = document.getElementById("genders");
+  localStorage.setItem("genders", ginput.value);
+  var getgenders = localStorage.getItem("genders");
+  console.log(getgenders);
+
+
+
+  if (document.getElementById("femaleOutfit1").checked == true){
+    document.getElementById("femaleOutfit1").value;
+    var fo1input = document.getElementById("femaleOutfit1");
+    localStorage.setItem("femaleOutfit1", fo1input.value);
+    var getfemaleOutfit1 = localStorage.getItem("femaleOutfit1");
+    console.log(getfemaleOutfit1);
+  }
+  else if (document.getElementById("femaleOutfit2").checked == true){
+    document.getElementById("femaleOutfit2").value;
+    var fo2input = document.getElementById("femaleOutfit2");
+    localStorage.setItem("femaleOutfit2", fo2input.value);
+    var getfemaleOutfit2 = localStorage.getItem("femaleOutfit2");
+    console.log(getfemaleOutfit2);
+  }
+  else if (document.getElementById("femaleOutfit3").checked == true){
+    document.getElementById("femaleOutfit3").value;
+    var fo3input = document.getElementById("femaleOutfit3");
+    localStorage.setItem("femaleOutfit3", fo3input.value);
+    var getfemaleOutfit3 = localStorage.getItem("femaleOutfit3");
+    console.log(getfemaleOutfit3);
+  }
+  else if (document.getElementById("femaleOutfit4").checked == true){
+    document.getElementById("femaleOutfit4").value;
+    var fo4input = document.getElementById("femaleOutfit4");
+    localStorage.setItem("femaleOutfit4", fo4input.value);
+    var getfemaleOutfit4 = localStorage.getItem("femaleOutfit4");
+    console.log(getfemaleOutfit4);
+  }
+  else if (document.getElementById("femaleOutfit5").checked == true){
+    document.getElementById("femaleOutfit5").value;
+    var fo5input = document.getElementById("femaleOutfit5");
+    localStorage.setItem("femaleOutfit5", fo5input.value);
+    var getfemaleOutfit5 = localStorage.getItem("femaleOutfit5");
+    console.log(getfemaleOutfit5);
+  }
+  else if (document.getElementById("femaleOutfit6").checked == true){
+    document.getElementById("femaleOutfit6").value;
+    var fo6input = document.getElementById("femaleOutfit6");
+    localStorage.setItem("femaleOutfit6", fo6input.value);
+    var getfemaleOutfit6 = localStorage.getItem("femaleOutfit6");
+    console.log(getfemaleOutfit6);
+  }
+
+
+
+
+  if (document.getElementById("maleOutfit1").checked == true){
+    document.getElementById("maleOutfit1").value;
+    var mo1input = document.getElementById("maleOutfit1");
+    localStorage.setItem("maleOutfit1", mo1input.value);
+    var getmaleOutfit1 = localStorage.getItem("maleOutfit1");
+    console.log(getmaleOutfit1);
+  }
+  else if (document.getElementById("maleOutfit2").checked == true){
+    document.getElementById("maleOutfit2").value;
+    var mo2input = document.getElementById("maleOutfit2");
+    localStorage.setItem("maleOutfit2", mo2input.value);
+    var getmaleOutfit2 = localStorage.getItem("maleOutfit2");
+    console.log(getmaleOutfit2);
+  }
+  else if (document.getElementById("maleOutfit3").checked == true){
+    document.getElementById("maleOutfit3").value;
+    var mo3input = document.getElementById("maleOutfit3");
+    localStorage.setItem("maleOutfit3", mo3input.value);
+    var getmaleOutfit3 = localStorage.getItem("maleOutfit3");
+    console.log(getmaleOutfit3);
+  }
+  else if (document.getElementById("maleOutfit4").checked == true){
+    document.getElementById("maleOutfit4").value;
+    var mo4input = document.getElementById("maleOutfit4");
+    localStorage.setItem("maleOutfit4", mo4input.value);
+    var getmaleOutfit4 = localStorage.getItem("maleOutfit4");
+    console.log(getmaleOutfit4);
+  }
+  else if (document.getElementById("maleOutfit5").checked == true){
+    document.getElementById("maleOutfit5").value;
+    var mo5input = document.getElementById("maleOutfit5");
+    localStorage.setItem("maleOutfit5", mo5input.value);
+    var getmaleOutfit5 = localStorage.getItem("maleOutfit5");
+    console.log(getmaleOutfit5);
+  }
+  else if (document.getElementById("maleOutfit6").checked == true){
+    document.getElementById("maleOutfit6").value;
+    var mo6input = document.getElementById("maleOutfit6");
+    localStorage.setItem("maleOutfit6", mo6input.value);
+    var getmaleOutfit6 = localStorage.getItem("maleOutfit6");
+    console.log(getmaleOutfit6);
+  }
+
+
+
+  if (document.getElementById("h1").checked == true){
+    document.getElementById("h1").value;
+    var h1input = document.getElementById("h1");
+    localStorage.setItem("h1", h1input.value);
+    var geth1 = localStorage.getItem("h1");
+    console.log(geth1);
+  }
+  else if (document.getElementById("h2").checked == true){
+    document.getElementById("h2").value;
+    var h2input = document.getElementById("h2");
+    localStorage.setItem("h2", h2input.value);
+    var geth2 = localStorage.getItem("h2");
+    console.log(geth2);
+  }
+  else if (document.getElementById("h3").checked == true){
+    document.getElementById("h3").value;
+    var h3input = document.getElementById("h3");
+    localStorage.setItem("h3", h3input.value);
+    var geth3 = localStorage.getItem("h3");
+    console.log(geth3);
+  }
+
+
+  if (document.getElementById("b1").checked == true){
+    document.getElementById("b1").value;
+    var b1input = document.getElementById("b1");
+    localStorage.setItem("b1", b1input.value);
+    var getb1 = localStorage.getItem("b1");
+    console.log(getb1);
+  }
+  else if (document.getElementById("b2").checked == true){
+    document.getElementById("b2").value;
+    var b2input = document.getElementById("b2");
+    localStorage.setItem("b2", b2input.value);
+    var getb2 = localStorage.getItem("b2");
+    console.log(getb2);
+  }
+  else if (document.getElementById("b3").checked == true){
+    document.getElementById("b3").value;
+    var b3input = document.getElementById("b3");
+    localStorage.setItem("b3", b3input.value);
+    var getb3 = localStorage.getItem("b3");
+    console.log(getb3);
+  }
+  else if (document.getElementById("b4").checked == true){
+    document.getElementById("b4").value;
+    var b4input = document.getElementById("b4");
+    localStorage.setItem("b4", b4input.value);
+    var getb4 = localStorage.getItem("b4");
+    console.log(getb4);
+  }
+  else if (document.getElementById("b5").checked == true){
+    document.getElementById("b5").value;
+    var b5input = document.getElementById("b5");
+    localStorage.setItem("b5", b5input.value);
+    var getb5 = localStorage.getItem("b5");
+    console.log(getb5);
+  }
+  else if (document.getElementById("b6").checked == true){
+    document.getElementById("b6").value;
+    var b6input = document.getElementById("b6");
+    localStorage.setItem("b6", b6input.value);
+    var getb6 = localStorage.getItem("b6");
+    console.log(getb6);
+  }
+
+
+
+  if (document.getElementById("c1").checked == true){
+    document.getElementById("c1").value;
+    var c1input = document.getElementById("c1");
+    localStorage.setItem("c1", c1input.value);
+    var getc1 = localStorage.getItem("c1");
+    console.log(getc1);
+  }
+  else if (document.getElementById("c2").checked == true){
+    document.getElementById("c2").value;
+    var c2input = document.getElementById("c2");
+    localStorage.setItem("c2", c2input.value);
+    var getc2 = localStorage.getItem("c2");
+    console.log(getc2);
+  }
+  else if (document.getElementById("c3").checked == true){
+    document.getElementById("c3").value;
+    var c3input = document.getElementById("c3");
+    localStorage.setItem("c3", c3input.value);
+    var getc3 = localStorage.getItem("c3");
+    console.log(getc3);
+  }
+  else if (document.getElementById("c4").checked == true){
+    document.getElementById("c4").value;
+    var c4input = document.getElementById("c4");
+    localStorage.setItem("c4", c4input.value);
+    var getc4 = localStorage.getItem("c4");
+    console.log(getc4);
+  }
+  else if (document.getElementById("c5").checked == true){
+    document.getElementById("c5").value;
+    var c5input = document.getElementById("c5");
+    localStorage.setItem("c5", c5input.value);
+    var getc5 = localStorage.getItem("c5");
+    console.log(getc5);
+  }
+
+
+
+  if (document.getElementById("sh1").checked == true){
+    document.getElementById("sh1").value;
+    var sh1input = document.getElementById("sh1");
+    localStorage.setItem("sh1", sh1input.value);
+    var getsh1 = localStorage.getItem("sh1");
+    console.log(getsh1);
+  }
+  else if (document.getElementById("sh2").checked == true){
+    document.getElementById("sh2").value;
+    var sh2input = document.getElementById("sh2");
+    localStorage.setItem("sh2", sh2input.value);
+    var getsh2 = localStorage.getItem("sh2");
+    console.log(getsh2);
+  }
+  else if (document.getElementById("sh3").checked == true){
+    document.getElementById("sh3").value;
+    var sh3input = document.getElementById("sh3");
+    localStorage.setItem("sh3", sh3input.value);
+    var getsh3 = localStorage.getItem("sh3");
+    console.log(getsh3);
+  }
+  else if (document.getElementById("sh4").checked == true){
+    document.getElementById("sh4").value;
+    var sh4input = document.getElementById("sh4");
+    localStorage.setItem("sh4", sh4input.value);
+    var getsh4 = localStorage.getItem("sh4");
+    console.log(getsh4);
+  }
+  else if (document.getElementById("sh5").checked == true){
+    document.getElementById("sh5").value;
+    var sh5input = document.getElementById("sh5");
+    localStorage.setItem("sh5", sh5input.value);
+    var getsh5 = localStorage.getItem("sh5");
+    console.log(getsh5);
+  }
+  if (document.getElementById("sh6").checked == true){
+    document.getElementById("sh6").value;
+    var sh6input = document.getElementById("sh6");
+    localStorage.setItem("sh6", sh6input.value);
+    var getsh6 = localStorage.getItem("sh6");
+    console.log(getsh6);
+  }
+  else if (document.getElementById("sh7").checked == true){
+    document.getElementById("sh7").value;
+    var sh7input = document.getElementById("sh7");
+    localStorage.setItem("sh7", sh7input.value);
+    var getsh7 = localStorage.getItem("sh7");
+    console.log(getsh7);
+  }
+  else if (document.getElementById("sh8").checked == true){
+    document.getElementById("sh8").value;
+    var sh8input = document.getElementById("sh8");
+    localStorage.setItem("sh8", sh8input.value);
+    var getsh8 = localStorage.getItem("sh8");
+    console.log(getsh8);
+  }
+  else if (document.getElementById("sh9").checked == true){
+    document.getElementById("sh9").value;
+    var sh9input = document.getElementById("sh9");
+    localStorage.setItem("sh9", sh9input.value);
+    var getsh9 = localStorage.getItem("sh9");
+    console.log(getsh9);
+  }
+  else if (document.getElementById("sh10").checked == true){
+    document.getElementById("sh10").value;
+    var sh10input = document.getElementById("sh10");
+    localStorage.setItem("sh10", sh10input.value);
+    var getsh10 = localStorage.getItem("sh10");
+    console.log(getsh10);
+  }
+
+
+
+
+  if (document.getElementById("si1").checked == true){
+    document.getElementById("si1").value;
+    var si1input = document.getElementById("si1");
+    localStorage.setItem("si1", si1input.value);
+    var getsi1 = localStorage.getItem("si1");
+    console.log(getsi1);
+  }
+  else if (document.getElementById("si2").checked == true){
+    document.getElementById("si2").value;
+    var si2input = document.getElementById("si2");
+    localStorage.setItem("si2", si2input.value);
+    var getsi2 = localStorage.getItem("si2");
+    console.log(getsi2);
+  }
+  else if (document.getElementById("si3").checked == true){
+    document.getElementById("si3").value;
+    var si3input = document.getElementById("si3");
+    localStorage.setItem("si3", si3input.value);
+    var getsi3 = localStorage.getItem("si3");
+    console.log(getsi3);
+  }
+  else if (document.getElementById("si4").checked == true){
+    document.getElementById("si4").value;
+    var si4input = document.getElementById("si4");
+    localStorage.setItem("si4", si4input.value);
+    var getsi4 = localStorage.getItem("si4");
+    console.log(getsi4);
+  }
+  else if (document.getElementById("si5").checked == true){
+    document.getElementById("si5").value;
+    var si5input = document.getElementById("si5");
+    localStorage.setItem("si5", si5input.value);
+    var getsi5 = localStorage.getItem("si5");
+    console.log(getsi5);
+  }
+  if (document.getElementById("si6").checked == true){
+    document.getElementById("si6").value;
+    var si6input = document.getElementById("si6");
+    localStorage.setItem("si6", si6input.value);
+    var getsi6 = localStorage.getItem("si6");
+    console.log(getsi6);
+  }
+  else if (document.getElementById("si7").checked == true){
+    document.getElementById("si7").value;
+    var si7input = document.getElementById("si7");
+    localStorage.setItem("si7", si7input.value);
+    var getsi7 = localStorage.getItem("si7");
+    console.log(getsi7);
+  }
+  else if (document.getElementById("si8").checked == true){
+    document.getElementById("si8").value;
+    var si8input = document.getElementById("si8");
+    localStorage.setItem("si8", si8input.value);
+    var getsi8 = localStorage.getItem("si8");
+    console.log(getsi8);
+  }
+  else if (document.getElementById("si9").checked == true){
+    document.getElementById("si9").value;
+    var si9input = document.getElementById("si9");
+    localStorage.setItem("si9", si9input.value);
+    var getsi9 = localStorage.getItem("si9");
+    console.log(getsi9);
+  }
+  else if (document.getElementById("si10").checked == true){
+    document.getElementById("si10").value;
+    var si10input = document.getElementById("si10");
+    localStorage.setItem("si10", si10input.value);
+    var getsi10 = localStorage.getItem("si10");
+    console.log(getsi10);
+  }
+  if (document.getElementById("si11").checked == true){
+    document.getElementById("si11").value;
+    var si11input = document.getElementById("si11");
+    localStorage.setItem("si11", si11input.value);
+    var getsi11 = localStorage.getItem("si11");
+    console.log(getsi11);
+  }
+  else if (document.getElementById("si12").checked == true){
+    document.getElementById("si12").value;
+    var si12input = document.getElementById("si12");
+    localStorage.setItem("si12", si12input.value);
+    var getsi12 = localStorage.getItem("si12");
+    console.log(getsi12);
+  }
+  else if (document.getElementById("si13").checked == true){
+    document.getElementById("si13").value;
+    var si13input = document.getElementById("si13");
+    localStorage.setItem("si13", si13input.value);
+    var getsi13 = localStorage.getItem("si13");
+    console.log(getsi13);
+  }
+  else if (document.getElementById("si14").checked == true){
+    document.getElementById("si14").value;
+    var si14input = document.getElementById("si14");
+    localStorage.setItem("si14", si14input.value);
+    var getsi14 = localStorage.getItem("si14");
+    console.log(getsi14);
+  }
+
+
+
+}
